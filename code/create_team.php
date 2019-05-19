b3da3b29406023553080333f9da335bf6d289cd3
NaisilaPuka <nicypp@gmail.com>
2019-05-20 01:45:48 +0300

    Add create team procedure

--- code/create_team.php
+++ code/create_team.php
@@ -8,52 +8,23 @@
   include('utils/config.php');
 
   $name = $_POST['name'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $address = $_POST['address'];
-  $phone = $_POST['phone'];
-  $organization = $_POST['organization'];
-
-  $name = mysqli_real_escape_string($conn, $name);
-  $email = mysqli_real_escape_string($conn, $email);
-  $password = mysqli_real_escape_string($conn, $password);
-  $address = mysqli_real_escape_string($conn, $address);
-  $phone = mysqli_real_escape_string($conn, $phone);
-  $organization = mysqli_real_escape_string($conn, $organization);
+  $userId = $_POST['userId'];
+  $affiliation = $_POST['affiliation'];
+  $teamKey = $_POST['key'];
 
   $name = (string) $name;
-  $email = (string) $email;
-  $password = (string) $password;
-  $address = (string) $address;
-  $phone = (string) $phone;
-  $organization = (string) $organization;
+  $userId = (string) $userId;
+  $affiliation = (string) $affiliation;
+  $teamKey = (string) $teamKey;
 
-  $reg_query = "INSERT INTO BasicUser(name, email, password, address) VALUES ( '$name', '$email', '$password', '$address');";
-
-
-  //$reg_result = mysqli_query($conn, $reg_query);
-  
-  //$row = mysqli_fetch_array($reg_result);
+  $reg_query = "INSERT INTO Team(name, supervisor, affiliation, teamKey) VALUES ( '$name', '$userId', '$affiliation', '$teamKey');";
 
 
   if(mysqli_query($conn, $reg_query))
   {
-    $user_id = mysqli_insert_id($conn);
+    $teamId = mysqli_insert_id($conn);
     $result->status = "success";
-    $result->userID = $user_id;
-
-    $add_phone = "INSERT INTO Phone(userID, phoneNumber) VALUES ('$user_id', '$phone')";
-    mysqli_query($conn, $add_phone);
-    //not a superuser
-    if(empty($organization))
-    {
-    }
-    //superuser
-    else
-    {
-      $add_superuser = "INSERT INTO SuperUser(userID, organization) VALUES ('$user_id', '$organization')";
-      mysqli_query($conn, $add_superuser);
-    }
+    $result->teamId = $teamId;
   }
   else
   {
