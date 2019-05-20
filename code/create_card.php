f6dd6a4d71a49427a045055eb815285e00914369
Efronova <efronovak@gmail.com>
2019-05-20 17:27:32 +0300

    create card

---
+++ code/create_card.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Creating card 
+ * @author Kunduz Efronova
+ * @version 20/05/2019
+ */
+
+  include('utils/config.php');
+
+  $name = $_POST['name'];
+  $priority = $_POST['priority'];
+  $description = $_POST['description'];
+  $dueDate = $_POST['dueDate'];
+  $archived = $_POST['archived'];
+  $finished = $_POST['finished'];
+  $listId = $_POST['listId'];
+ 
+  $name = (string) $name;
+  $finished = (string) $finished;
+  $archived = (string) $archived;
+  $description = (string) $description;
+  $priority = (string) $priority;
+  $listId = (string) $listId;
+  $dueDate = (string) $dueDate;
+
+  $reg_query = "INSERT INTO List(name, priority, description, dueDate, listID, archived, finished) VALUES ( '$name', '$priority', '$description', '$dueDate', '$listId', '$archived', '$finished');";
+
+  if(mysqli_query($conn, $reg_query))
+  {
+    $cardId = mysqli_insert_id($conn);
+    $result->status = "success";
+    $result->cardId = $cardId;
+  }
+  else
+  {
+    $result->status = "fail";
+    $result->listId = "";
+  }
+
+  //{"status": "success", "userID": "12345"}
+
+  $json_res = json_encode($result);
+  echo $json_res;
+?>
