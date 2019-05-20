1ce00fdbc3b5c4b850b8e3648581633a8a1c566a
NaisilaPuka <nicypp@gmail.com>
2019-05-21 00:11:04 +0300

    Start get teams

---
+++ code/get_teams.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Create Board Procedure
+ * @author Naisila Puka
+ * @version 20/05/2019
+ */
+
+  include('utils/config.php');
+
+  //Give me inputs as in these POST functions
+  //-----------
+  $userId = $_POST['userId'];
+  //-----------
+
+  $userId = (string) $userId;
+
+  $reg_query = "INSERT INTO Board(name, description, priority, color, requirements, estimatedTime, ownerID) VALUES ( '$name', '$description', '$priority', '$color', '$requirements', '$estimatedTime', '$ownerId');";
+
+  if(mysqli_query($conn, $reg_query))
+  {
+    $boardId = mysqli_insert_id($conn);
+    $result->status = "success";
+    $result->boardId = $boardId;
+
+    //insert into works on
+    $query = "INSERT INTO WorksOn(boardID, teamID) VALUES('$boardId', '$teamId');";
+    mysqli_query($conn, $query);
+  }
+  else
+  {
+    $result->status = "fail";
+    $result->boardId = "";
+  }
+
+  //{"status": "success", "boardId": "12345"}
+
+  $json_res = json_encode($result);
+  echo $json_res;
+?>
