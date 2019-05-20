296fceeee602fc26c56b5cfb52af32a053981040
Efronova <efronovak@gmail.com>
2019-05-20 19:14:14 +0300

    create attachment

--- code/create_attachment.php
+++ code/create_attachment.php
@@ -6,7 +6,7 @@
  */
 
   include('utils/config.php');
-attachmentID, name, size, uploadDate, description, attacher, relatedCard
+
   //Give me inputs as in these POST functions
   //-----------
   $name = $_POST['name'];
@@ -14,37 +14,35 @@ attachmentID, name, size, uploadDate, description, attacher, relatedCard
   $description = $_POST['description'];
   $attacher = $_POST['attacher'];
   $relatedCard = $_POST['relatedCard'];
-  
+
   //Caution: DATETIME IS IN THIS FORMAT '2000-12-31 23:59:59'
   $uploadDate = $_POST['uploadDate'];
 
-  
   //-----------
 
   $name = (string) $name;
-  $finished = (string) $finished;
-  $archived = (string) $archived;
+  $size = (string) $size;
   $description = (string) $description;
-  $priority = (string) $priority;
-  $listId = (string) $listId;
-  $dueDate = (string) $dueDate;
+  $attacher = (string) $attacher;
+  $relatedCard = (string) $relatedCard;
+  $uploadDate = (string) $uploadDate;
 
-  $reg_query = "INSERT INTO List(name, priority, description, dueDate, listID, archived, finished) VALUES ( '$name', '$priority', '$description', '$dueDate', '$listId', '$archived', '$finished');";
+  $reg_query = "INSERT INTO List(name, size, uploadDate, description, attacher, relatedCard) VALUES ( '$name', '$size','$uploadDate','$description', '$attacher', '$relatedCard');";
 
   if(mysqli_query($conn, $reg_query))
   {
-    $cardId = mysqli_insert_id($conn);
+    $attachmentId = mysqli_insert_id($conn);
     $result->status = "success";
-    $result->cardId = $cardId;
+    $result->attachmentId = $attachmentId;
   }
   else
   {
     $result->status = "fail";
-    $result->cardId = "";
+    $result->attachmentId = "";
   }
 
   //output in the form
-  //{"status": "success", "cardId": "12345"}
+  //{"status": "success", "attachmentId": "12345"}
 
   $json_res = json_encode($result);
   echo $json_res;
