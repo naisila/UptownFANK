5128160f844b9ea6be0d6467eb6a98d31ad368a5
NaisilaPuka <nicypp@gmail.com>
2019-05-20 17:38:21 +0300

    Specify date format and stuff

--- code/create_card.php
+++ code/create_card.php
@@ -7,14 +7,23 @@
 
   include('utils/config.php');
 
+  //Give me inputs as in these POST functions
+  //-----------
   $name = $_POST['name'];
   $priority = $_POST['priority'];
   $description = $_POST['description'];
+
+  //Caution: DATETIME IS IN THIS FORMAT '2000-12-31 23:59:59'
   $dueDate = $_POST['dueDate'];
+
+  //SHOULD BE EITHER 'True' or 'False'
   $archived = $_POST['archived'];
+
+  //SHOULD BE EITHER 'True' or 'False'
   $finished = $_POST['finished'];
   $listId = $_POST['listId'];
- 
+  //-----------
+
   $name = (string) $name;
   $finished = (string) $finished;
   $archived = (string) $archived;
@@ -37,7 +46,8 @@
     $result->cardId = "";
   }
 
-  //{"status": "success", "boardID": "12345"}
+  //output in the form
+  //{"status": "success", "cardId": "12345"}
 
   $json_res = json_encode($result);
   echo $json_res;
